4e45eb6e28fc3da563afbb01a960caab40b52243
Ye-weibo <614237922@qq.com>
2017-07-18 18:26:56 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <div id="app"> -->
+    <!-- <router-link to="/article">home</router-link> -->
+    <!-- <router-link to="/hello">hello</router-link> -->
+    <router-view></router-view>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+  }
+</style>
+
